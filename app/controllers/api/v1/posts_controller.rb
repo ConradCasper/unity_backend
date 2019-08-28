2ece111dd92b0ec0148878ccae8d8e484ddae605
github username <github email address>
2019-08-28 10:27:01 -0400

    post model and controller done for now

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,59 @@
+class Api::V1::PostsController < ApplicationController
+    
+
+    def index
+        @posts = Post.all
+        render json: @posts.to_json( include: [:comments, :commenting_users, :likes, :liking_users] )
+    end
+
+    def create
+        @post = Post.create(post_params)
+        if @post.valid?
+            render json: { post: @post }, status: :created
+        else
+            render json: { error: 'failed to create post' }, status: :not_acceptable
+        end
+    end
+
+
+    def edit
+        @post = Post.find_by(id: params[:id])
+        render json: @post
+    end
+
+
+    def update
+        @post = Post.find_by(id: params[:id])
+        if @post.user_id === current_user.id
+        @post.content = post_params[:content]
+        @post.type = post_params[:type]
+        @post.img_url = post_params[:img_url]
+        @post.save
+        redirect_to `/api/v1/profile`
+        else
+            render json: { error: "you can't edit someone else's post" }, status: :unauthorized
+            redirect_to `/api/v1/profile`
+        end
+    end
+
+
+    def destroy
+        post = Post.find_by(id: params[:id])
+        post.destroy
+        redirect_to `/api/v1/profile`
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content, :type, :img_url).merge(user_id: current_user.id)
+    end
+        
+end
+
+
+    
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :posts, only: [:create, :edit, :update, :destroy, :index]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
